390756b55b239a84d8a80064b8e29ac972fa9a4e
amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>
2024-10-19 02:09:38 +0500

    Update 0-MEME.js

--- modules/commands/0-MEME.js
+++ modules/commands/0-MEME.js
@@ -277,6 +277,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/4nFOS0w.jpg" ,
 "https://i.imgur.com/tC2Sy8a.jpg" ,
      ];
-     var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠r 𝐒𝐡𝐚𝐧𝐤𝐚𝐫 𝐬𝐮𝐦𝐚𝐧 💝`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
+     var callback = () => api.sendMessage({body:`💝 Owner Mian Amir 💝`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/AOV.jpg")).on("close",() => callback());
    };
